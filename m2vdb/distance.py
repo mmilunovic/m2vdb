8ffe7dce8addab605fb5d3485dcc5049dc1853f1
mmilunovic <milunovicmilos@gmail.com>
2025-04-08 14:19:34 +0200

    IP distance

--- m2vdb/distance.py
+++ m2vdb/distance.py
@@ -37,3 +37,17 @@ def cosine_similarity(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     a_norm = a / np.linalg.norm(a, axis=1, keepdims=True)
     b_norm = b / np.linalg.norm(b, axis=1, keepdims=True)
     return 1 - np.dot(a_norm, b_norm.T)
+
+def ip_distance(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """
+    Compute Inner Product (IP) distance between two sets of vectors efficiently.
+    IP distance is defined as the negative dot product between vectors.
+    
+    Args:
+        a: First set of vectors (n x d)
+        b: Second set of vectors (m x d)
+    Returns:
+        Distance matrix (n x m)
+    """
+    # Compute negative dot product
+    return -np.dot(a, b.T)  # (n x m)
